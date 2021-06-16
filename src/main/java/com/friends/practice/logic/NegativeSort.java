f88ba7d4c7146b349d41ba63f4fd9a6cb7713429
Venkatesh Urlana <venkatesh_urlana@epam.com>
2021-06-16 21:36:34 +0530

    first task

---
+++ src/main/java/com/friends/practice/logic/NegativeSort.java
@@ -0,0 +1,18 @@
+package com.friends.practice.logic;
+
+public class NegativeSort {
+	//Array with sorted integers.program to square the array values and output a sorted array 
+	/// u should't create a array
+
+	
+	// [-5,-2,0,1,2,3] ==> [0,1,4,4,9,25]
+	
+	//[2,-3,7,6,1]   -- > [-3,1,2,6,7]
+	
+						//[1,4,9,36,49]
+					
+	public static void main(String[] args) {
+		
+	}
+
+}
